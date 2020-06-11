1b6a37b831581486cfc04c9fe60c4f926380fcc1
airwiggler <deardeer1998@gmail.com>
2020-06-11 20:03:16 +0300

    add some conditioned messages for LOLz

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,24 @@
 import random
-def main():
-    roll = random.randint(1,6)
-    print(f'You rolled a {roll}')
 
+def main():
+    dice_rolls = 25
+    dice_sum = 0
+    count = 0
+    for i in range(0, dice_rolls):
+        if dice_sum < 100:
+            roll = random.randint(1,6)
+            dice_sum += roll
+            count += 1
+            if roll == 1:
+                print(f'You rolled a {roll}, thats something')
+            elif roll == 6:
+                print(f'You rolled a {roll}, wowzers!')
+            elif roll % 2 != 1:
+                print(f'you rolled a {roll}, you can even split it evenly')
+            else:
+                print(f'You rolled a {roll}, try not to share it')
+        else:
+            pass
+    print(f"Your total is {dice_sum} in {count} rolls. not bad!")
 if __name__== "__main__":
   main()
